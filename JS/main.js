7bbb6750410158403997bb0408167f56c075c830
danishafahma <saniadanishafahma084@gmail.com>
2025-03-18 13:42:43 +0700

    [FEAT] first commit

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+// console.log("haloo duniaa");
+// document.write("sayaa senengg bgtzz")
+
+let nama = "Danisha";
+console.log(nama);
+console.log(typeof(nama))
+nama = "Nadiz";
+console.log(nama);
+console.log(typeof(nama))
+nama = 1234567890;
+console.log(nama);
+console.log(typeof(nama))
+let isFemale = false;
+console.log(typeof(isFemale))
+
+
+// let isFemale = false;
+
+var nama1;
+// const nama2
